9241732efcb183cdbe1391157fa0215fef2d09e0
todo-listing <anjalibatra591@gmail.com>
2020-05-05 23:02:02 +0530

    Updates

---
+++ precache-manifest.d7c014688d29c4e6a872057bf54dea9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb3fef389c420d36c9d031ec7dfccbf",
+    "url": "/e-portfolio/index.html"
+  },
+  {
+    "revision": "92553c49d40065cc55c6",
+    "url": "/e-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "bae10308c55c8534e400",
+    "url": "/e-portfolio/static/css/main.9f5b72bc.chunk.css"
+  },
+  {
+    "revision": "92553c49d40065cc55c6",
+    "url": "/e-portfolio/static/js/2.a1f4213c.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/e-portfolio/static/js/2.a1f4213c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae10308c55c8534e400",
+    "url": "/e-portfolio/static/js/main.a4e9987a.chunk.js"
+  },
+  {
+    "revision": "c702b84af155c04f1d98",
+    "url": "/e-portfolio/static/js/runtime-main.bdb9dd77.js"
+  },
+  {
+    "revision": "97a8a7723dcc3bd35baf27dc6b2c0a0a",
+    "url": "/e-portfolio/static/media/img.97a8a772.jpg"
+  }
+]);
